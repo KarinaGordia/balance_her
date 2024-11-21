86b193bbd8b675da5fdc33e95afd3215cb553b44
Karina Gordia <kgordya24@gmail.com>
2024-11-21 13:49:59 +0100

    resources adding

---
+++ lib/resources/images.dart
@@ -0,0 +1,13 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String cardsHeart = 'assets/images/cards_heart.svg';
+  static const String checkRounded = 'assets/images/check_rounded.svg';
+  static const String home = 'assets/images/home.svg';
+  static const String overviewStar = 'assets/images/overview_star.svg';
+  static const String run = 'assets/images/run.svg';
+  static const String startCircle = 'assets/images/start_circle.svg';
+  static const String welcomeSquare = 'assets/images/welcome_square.svg';
+}
