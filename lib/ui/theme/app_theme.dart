3f26663573431056cccfe9568fb479f19a60cdc2
Karina Gordia <kgordya24@gmail.com>
2024-11-21 16:52:35 +0100

    theme setup 2

--- lib/ui/theme/app_theme.dart
+++ lib/ui/theme/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:balance_her/resources/fonts.dart';
+import 'package:balance_her/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 abstract class AppTheme {
@@ -63,20 +64,3 @@ abstract class AppTheme {
   );
 }
 
-abstract class AppColors {
-  static const white = Color(0xffffffff);
-  static const background = Color(0xfffaf6f0);
-  static const surface = Color(0x14ffffff);
-  static const onSurface = Color(0x33F2AA91);
-  static const primary = Color(0xfff2aa91);
-  static const onPrimary = Color(0x6629282F);
-  static const secondary = Color(0xff29282f);
-  static const onSecondary = Color(0x66FAFAFA);
-  static const outline8 = Color(0x1429282F);
-  static const outline12 = Color(0x1E29282F);
-  static const outline_2 = Color(0xfffd886e);
-  static const red = Color(0xfffd886e);
-  static const violet = Color(0xffceb5f2);
-  static const blue = Color(0xff6ec1fd);
-  static const green = Color(0xffa2e492);
-}
