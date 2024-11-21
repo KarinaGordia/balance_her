540c84c8401ba4be303e9f39af75d0773a6130f5
Karina Gordia <kgordya24@gmail.com>
2024-11-21 14:36:20 +0100

    png images and fonts

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -6,8 +6,8 @@ class Images {
   static const String cardsHeart = 'assets/images/cards_heart.svg';
   static const String checkRounded = 'assets/images/check_rounded.svg';
   static const String home = 'assets/images/home.svg';
-  static const String overviewStar = 'assets/images/overview_star.svg';
+  static const String overviewStar = 'assets/images/overview_star.png';
   static const String run = 'assets/images/run.svg';
-  static const String startCircle = 'assets/images/start_circle.svg';
-  static const String welcomeSquare = 'assets/images/welcome_square.svg';
+  static const String startCircle = 'assets/images/start_circle.png';
+  static const String welcomeSquare = 'assets/images/welcome_square.png';
 }
