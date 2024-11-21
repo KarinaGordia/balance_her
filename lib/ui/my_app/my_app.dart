3cbe7a6f19794e544fa81a65fa0bbc0ac566ffab
Karina Gordia <kgordya24@gmail.com>
2024-11-21 11:02:46 +0100

    add colors

---
+++ lib/ui/my_app/my_app.dart
@@ -0,0 +1,28 @@
+import 'package:balance_her/ui/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BalanceHer',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/ui/theme/app_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// abstract class AppTheme {
+//   static final light = ColorScheme.fromSeed(
+//     seedColor: const Color(0xfffd886e),
+//     background: const Color(0xfffaf6f0),
+//     surface: const Color(0x14ffffff),
+//     onSurface:const Color(0x33F2AA91),
+//     secondary: const Color(0xff29282f),
+//     onSecondary: const Color(0x66FAFAFA),
+//     primary: const Color(0xfff2aa91),
+//     onPrimary: const Color(0x6629282F),
+//     outline: const Color(0x1E29282F),
+//   );
+// }
+
+abstract class AppColors {
+  static const background = Color(0xfffaf6f0);
+  static const surface = Color(0x14ffffff);
+  static const onSurface = Color(0x33F2AA91);
+  static const primary = Color(0xfff2aa91);
+  static const onPrimary = Color(0x6629282F);
+  static const secondary = Color(0xff29282f);
+  static const onSecondary = Color(0x66FAFAFA);
+  static const outline8 = Color(0x1429282F);
+  static const outline12 = Color(0x1E29282F);
+  static const outline_2 = Color(0xfffd886e);
+  static const red = Color(0xfffd886e);
+  static const violet = Color(0xffceb5f2);
+  static const blue = Color(0xff6ec1fd);
+  static const green = Color(0xffa2e492);
+}
