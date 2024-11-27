c141ebf819f729e269a05578673d9e91f7b4ae2f
Karina Gordia <kgordya24@gmail.com>
2024-11-27 14:16:25 +0100

    add assets

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -5,9 +5,13 @@ class Images {
 
   static const String cardsHeart = 'assets/images/cards_heart.svg';
   static const String checkRounded = 'assets/images/check_rounded.svg';
+  static const String habitsTab = 'assets/images/habits_tab.png';
+  static const String healthTab = 'assets/images/health_tab.png';
   static const String home = 'assets/images/home.svg';
+  static const String homeTab = 'assets/images/home_tab.png';
   static const String overviewStar = 'assets/images/overview_star.png';
   static const String run = 'assets/images/run.svg';
+  static const String settingsTab = 'assets/images/settings_tab.png';
   static const String startCircle = 'assets/images/start_circle.png';
   static const String welcomeSquare = 'assets/images/welcome_square.png';
 }
